3b83de7c571122d19ab340ebfaa8b926891a363b
marianasauer <marianasauer2004@gmail.com>
2024-12-11 11:27:16 -0300

    Initial Project

---
+++ app/src/main/java/com/example/nearby/ui/component/category/NearbyFilterChip.kt
@@ -0,0 +1,15 @@
+package com.example.nearby.ui.component.category
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NearbyCategoryFilterChip(
+    modifier: Modifier = Modifier,
+    category: NearbyCategory,
+    isSelected: Boolean,
+    onClick: (isSelected: Boolean) -> Unit
+
+){
+    
+}
