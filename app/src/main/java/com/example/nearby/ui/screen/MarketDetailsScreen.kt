ab0638d0a1aedbb8ee84795b2f10ca101aa2e41b
marianasauer <marianasauer2004@gmail.com>
2024-12-15 16:46:37 -0300

    Adding coil lib to images

---
+++ app/src/main/java/com/example/nearby/ui/screen/MarketDetailsScreen.kt
@@ -0,0 +1,18 @@
+package com.example.nearby.ui.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import com.example.nearby.data.model.Market
+
+@Composable
+fun MarketDetailsScreen(modifier: Modifier = Modifier, market: Market) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ){
+
+    }
+
+}
