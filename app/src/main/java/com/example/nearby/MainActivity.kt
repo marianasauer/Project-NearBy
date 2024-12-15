cb5278d90c4156aeea5cbb19eb151774dff1e769
marianasauer <marianasauer2004@gmail.com>
2024-12-15 16:31:04 -0300

    Creating Home Screen

--- app/src/main/java/com/example/nearby/MainActivity.kt
+++ app/src/main/java/com/example/nearby/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.nearby.ui.screen.HomeScreen
 import com.example.nearby.ui.theme.NearbyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NearbyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
